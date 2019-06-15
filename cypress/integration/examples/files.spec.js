ec111ae9e0180235bc1e2b2d5e448155fe3c123c
RenYing <yren@thoughtworks.com>
2019-06-15 18:57:09 +0800

    change port number

--- cypress/integration/examples/files.spec.js
+++ cypress/integration/examples/files.spec.js
@@ -7,7 +7,7 @@ const requiredExample = require('../../fixtures/example')
 
 context('Files', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:8080/commands/files')
+    cy.visit('http://localhost:8081/commands/files')
   })
 
   beforeEach(() => {
